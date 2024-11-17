5cfaf59a19206cea3516a1cc9fb79c8f0c2376a4
banushasarma <sharmabanu03@gmail.com>
2024-11-17 14:49:28 +0530

    Base serivice configured for access services

---
+++ Mango.Web/Utility/SD.cs
@@ -0,0 +1,16 @@
+﻿namespace Mango.Web.Utility
+{
+    public class SD
+    {
+        //Add Enum for ApiTypes
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE,
+            OPTIONS,
+            PATCH,
+        }
+    }
+}
