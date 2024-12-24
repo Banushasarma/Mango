8580658f2d5b7f696ad5bf32a7dc00cba0dc7e31
banushasarma <sharmabanu03@gmail.com>
2024-12-24 14:30:37 +0530

    All Auth DTOs added

---
+++ Mango.Service.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Service.AuthAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Mango.Service.AuthAPI/Models/Dto/RegistrationRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Service.AuthAPI.Models.Dto
+{
+    public class RegistrationRequestDto
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
