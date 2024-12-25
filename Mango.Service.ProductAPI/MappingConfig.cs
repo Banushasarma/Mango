0b7749184c02841e28471e923730a13f1284bb3b
banushasarma <sharmabanu03@gmail.com>
2024-12-25 21:43:08 +0530

    Product migration added and product data seeded.

---
+++ Mango.Service.ProductAPI/MappingConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Mango.Service.ProductAPI.Models;
+using Mango.Service.ProductAPI.Models.Dto;
+
+namespace Mango.Service.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mapperConfiguration = new MapperConfiguration(
+                config =>
+                {
+                    config.CreateMap<ProductDto, Product>();
+                    config.CreateMap<Product, ProductDto>();
+                }
+                );
+            return mapperConfiguration;
+        }
+    }
+}
