a15373c68f48953b430f54119fa4ab7c99f72981
banushasarma <sharmabanu03@gmail.com>
2024-12-25 13:54:43 +0530

    Login and Register integrated with UI views

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -11,10 +11,13 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
 builder.Services.AddHttpClient<ICouponService, CouponService>();
 
+//Configure service URLs
 SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
+SD.AuthAPIBase = builder.Configuration["ServiceUrls:AuthAPI"];
 
 builder.Services.AddScoped<IBaseService, BaseService>();
 builder.Services.AddScoped<ICouponService, CouponService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 var app = builder.Build();
 
--- Mango.Web/Utility/SD.cs
+++ Mango.Web/Utility/SD.cs
@@ -2,8 +2,15 @@
 {
     public class SD
     {
+        //Base URL for Auth API
+        public static string AuthAPIBase { get; set; }
+
         //Base URL for coupon API
         public static string CouponAPIBase { get; set; }
+
+        //Add static role
+        public const string RoleAdmin = "ADMIN";
+        public const string RoleCustomer = "CUSTOMER";
 
         //Add Enum for ApiTypes
         public enum ApiType
