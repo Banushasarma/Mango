3e03eaa8a7c561da5933f185c91243bdd10af7be
banushasarma <sharmabanu03@gmail.com>
2024-12-28 20:47:46 +0530

    Cart page completed

--- Mango.Web/Controllers/CartController.cs
+++ Mango.Web/Controllers/CartController.cs
@@ -33,5 +33,40 @@ namespace Mango.Web.Controllers
             }
             return new CartDto();
         }
+
+
+        public async Task<IActionResult> ApplyCoupon(CartDto cartDto)
+        {
+            ResponseDto response = await _cartService.ApplyCouponAsync(cartDto);
+            if (response != null)
+            {
+                TempData["success"] = "Coupon applied succesfully";
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+        }
+        public async Task<IActionResult> RemoveCoupon(CartDto cartDto)
+        {
+            cartDto.CartHeader.CouponCode = "";
+            ResponseDto response = await _cartService.ApplyCouponAsync(cartDto);
+            if (response != null)
+            {
+                TempData["success"] = "Coupon removed succesfully";
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Remove(int cartDetailsId)
+        {
+            var userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+            ResponseDto response = await _cartService.RemoveFromCartAsync(cartDetailsId);
+            if (response != null)
+            {
+                TempData["success"] = "Item removed from the cart";
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+        }
     }
 }
